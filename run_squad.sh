7755a0de88b08f917c05661946efbfb187fb8323
ben <bbclr20@gmail.com>
2020-11-13 16:03:06 +0800

    first commit

---
+++ run_squad.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+export SQUAD_DIR=./squad
+
+python run_squad.py \
+    --model_type distilbert \
+    --model_name_or_path distilbert-base-uncased \
+    --do_train \
+    --do_eval \
+    --version_2_with_negative \
+    --train_file $SQUAD_DIR/train-v2.0.json \
+    --predict_file $SQUAD_DIR/dev-v2.0.json \
+    --learning_rate 3e-5 \
+    --num_train_epochs 1 \
+    --max_seq_length 384 \
+    --doc_stride 128 \
+    --output_dir ./finetuned_squad/ \
+    --per_gpu_eval_batch_size=8  \
+    --per_gpu_train_batch_size=8   \
+    --save_steps 5000
