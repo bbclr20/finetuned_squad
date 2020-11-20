ce50b605537424798ceca25f086227284659c178
ben <bbclr20@gmail.com>
2020-11-20 17:02:57 +0800

    update download script

--- download.sh
+++ download.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
-mkdir squad
-wget https://rajpurkar.github.io/SQuAD-explorer/dataset/train-v2.0.json -O squad/train-v2.0.json
-wget https://rajpurkar.github.io/SQuAD-explorer/dataset/dev-v2.0.json -O squad/dev-v2.0.json
+SQUAD1="squad/squad1"
+mkdir -p $SQUAD1
+wget https://rajpurkar.github.io/SQuAD-explorer/dataset/train-v1.1.json -O ${SQUAD1}/train-v1.1.json
+wget https://rajpurkar.github.io/SQuAD-explorer/dataset/dev-v1.1.json -O ${SQUAD1}/dev-v1.1.json
+
+SQUAD2="squad/squad2"
+mkdir -p $SQUAD2
+wget https://rajpurkar.github.io/SQuAD-explorer/dataset/train-v2.0.json -O ${SQUAD2}/train-v2.0.json
+wget https://rajpurkar.github.io/SQuAD-explorer/dataset/dev-v2.0.json -O ${SQUAD2}/dev-v2.0.json
